13d3219cc7475d3c029408d1dbbab00caf0f33ca
Harry <harry810704@gmail.com>
2021-08-26 02:20:58 +0800

    Ready to deploy!

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 module.exports = {
     outputDir: 'dist',
     assetsDir: 'static',
-    baseUrl: IS_PRODUCTION
+    publicPath: IS_PRODUCTION
     ? 'https://harrywebapp.herokuapp.com/'
     : '/',
     // For Production, replace set baseUrl to CDN
