371cbf7c37937aaeb9391664b282bcdc1f592d8f
Harry <harry810704@gmail.com>
2021-08-26 12:08:04 +0800

    add webpack configuration

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  outputDir: 'dist',
+  assetsDir: 'static',
+  publicPath: IS_PRODUCTION
+    ? 'https://harrywebapp.herokuapp.com/'
+    : '/',
+  devServer: {
+    proxy: {
+      '/api*': {
+        // Forward frontend dev server request for /api to flask dev server
+        target: 'http://localhost:5000/'
+      }
+    }
+  },
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
