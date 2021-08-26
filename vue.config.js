431553e4a2daf372cd656f391b32336f241f8e79
Harry <harry810704@gmail.com>
2021-08-26 13:11:50 +0800

    add webpack configuration

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,6 @@ const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 const path = require('path')
 
 module.exports = {
-    // Template for index.html
-    index: path.resolve(__dirname, './dist/index.html'),
-    // Paths
-    assetsRoot: path.resolve(__dirname, './dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
     publicPath: IS_PRODUCTION
         ? 'https://harrywebapp.herokuapp.com/'
         : '/',
