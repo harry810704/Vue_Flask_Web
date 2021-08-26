11e296418aaf5c08549433aa245b98a2a8384ae5
Harry <harry810704@gmail.com>
2021-08-26 12:25:50 +0800

    add webpack configuration

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,6 @@ const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 const path = require('path')
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-    path: path.resolve(__dirname + '/dist'), // 打包生成檔案地址，必須是絕對路徑
-    filename: '[name].build.js', // 生成的檔名
-    },
-    mode: 'production',
     publicPath: IS_PRODUCTION
     ? 'https://harrywebapp.herokuapp.com/'
     : '/',
