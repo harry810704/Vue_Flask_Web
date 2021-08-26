24621b24682ad843cfbd1b388ec180b21bfab5b0
Harry <harry810704@gmail.com>
2021-08-26 12:19:23 +0800

    add webpack configuration

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,24 @@
-const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+// const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+// const path = require('path')
+// entry: './src/main.js',
+//   output: {
+//     path: path.resolve(__dirname + '/dist'), // 打包生成檔案地址，必須是絕對路徑
+//     filename: '[name].build.js', // 生成的檔名
+//   },
+//   publicPath: IS_PRODUCTION
+//     ? 'https://harrywebapp.herokuapp.com/'
+//     : '/',
+//   devServer: {
+//     proxy: {
+//       '/api*': {
+//         // Forward frontend dev server request for /api to flask dev server
+//         target: 'http://localhost:5000/'
+//       }
+//     }
+//   },
 
 module.exports = {
-  outputDir: 'dist',
-  assetsDir: 'static',
-  publicPath: IS_PRODUCTION
-    ? 'https://harrywebapp.herokuapp.com/'
-    : '/',
-  devServer: {
-    proxy: {
-      '/api*': {
-        // Forward frontend dev server request for /api to flask dev server
-        target: 'http://localhost:5000/'
-      }
-    }
-  },
+  mode: 'production',
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
